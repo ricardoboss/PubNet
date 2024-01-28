0e4a82a7966debcf872fcba576abab05eec0b637
Ricardo Boss <mail@ricardoboss.de>
2024-01-28 22:03:30 +0100

    Respect path base when rewriting

--- Web/PubNet.API/Middlewares/PubClientRewriterMiddleware.cs
+++ Web/PubNet.API/Middlewares/PubClientRewriterMiddleware.cs
@@ -1,31 +1,36 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace PubNet.API.Middlewares;
 
 public class PubClientRewriterMiddleware(ILogger<PubClientRewriterMiddleware> logger) : IMiddleware
 {
 	public Task InvokeAsync(HttpContext context, RequestDelegate next)
 	{
-		if (!MatchesPubClientRequest(context))
+		if (!MatchesPubClientRequest(context, out var path))
 			return next(context);
 
-		var path = context.Request.Path.Value;
-		if (path is null)
-			return next(context);
-
-		if (!path.StartsWith("/api/", StringComparison.OrdinalIgnoreCase))
-			return next(context);
-
-		// rewrite request from /api/ to /Packages/Dart/
-		context.Request.Path = path.Replace("/api/", "/Packages/Dart/", StringComparison.OrdinalIgnoreCase);
+		// rewrite request from /api/packages/ to /Packages/Dart/
+		context.Request.Path = path.Replace("/api/packages/", "/Packages/Dart/", StringComparison.OrdinalIgnoreCase);
 
 		logger.LogTrace("Rewrote request path from {OldPath} to {NewPath}", path, context.Request.Path.Value);
 
 		return next(context);
 	}
 
-	private static bool MatchesPubClientRequest(HttpContext context)
+	private static bool MatchesPubClientRequest(HttpContext context, [NotNullWhen(true)] out string? path)
 	{
-		var accept = context.Request.GetTypedHeaders().Accept;
+		path = context.Request.Path.Value;
+		if (path is null)
+			return false;
 
-		return accept.Any(x => x.MediaType.StartsWith("application/vnd.pub.v2+json", StringComparison.OrdinalIgnoreCase));
+		path = context.Request.PathBase + path;
+		if (!path.StartsWith("/api/packages/", StringComparison.OrdinalIgnoreCase))
+			return false;
+
+		var accept = context.Request.GetTypedHeaders().Accept;
+		if (!accept.Any(x => x.MediaType.StartsWith("application/vnd.pub.v2+json", StringComparison.OrdinalIgnoreCase)))
+			return false;
+
+		return true;
 	}
 }
