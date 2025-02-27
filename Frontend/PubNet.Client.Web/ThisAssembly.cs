7b3f862d349ba95ffdb0d8bc60c41fc79ed4e10c
Ricardo Boss <mail@ricardoboss.de>
2025-02-27 23:20:41 +0100

    deps: Use Nerdbank.GitVersioning to get git commit ids generated

---
+++ Frontend/PubNet.Client.Web/ThisAssembly.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[SuppressMessage("ReSharper", "CheckNamespace", Justification = "Assembly-level")]
+internal static partial class ThisAssembly
+{
+	public static string MajorMinorPatch { get; } = AssemblyVersion.Split('.').Take(3).Aggregate((a, b) => $"{a}.{b}");
+	public static string SemVer { get; } = AssemblyInformationalVersion.Split('+', 2).First();
+	public static string GitShortCommitId { get; } = GitCommitId[..7];
+}
