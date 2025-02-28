6b286b5aa78b3f291dbc2fb8c024a086706076cd
Ricardo Boss <mail@ricardoboss.de>
2025-02-28 04:46:19 +0100

    WIP admin interface

--- Backend/PubNet.API/Controllers/Admin/AdminController.cs
+++ Backend/PubNet.API/Controllers/Admin/AdminController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PubNet.API.Abstractions.Authentication;
 using PubNet.API.Attributes;
+using PubNet.API.DTO.Admin;
+using PubNet.API.DTO.Authentication;
 using PubNet.Auth.Models;
 
 namespace PubNet.API.Controllers.Admin;
@@ -8,6 +11,14 @@ namespace PubNet.API.Controllers.Admin;
 [ApiController]
 [Route("[controller]")]
 [Authorize, RequireRole(Role.Admin)]
-public class AdminController() : PubNetControllerBase
+public class AdminController(IAccountService accountService) : PubNetControllerBase
 {
+	[HttpPost("Account")]
+	public async Task<IdentityDto> CreateAccountAsync(CreateAccountDto data,
+		CancellationToken cancellationToken = default)
+	{
+		var identity = await accountService.CreateAccountAsync(data, cancellationToken);
+
+		return IdentityDto.MapFrom(identity);
+	}
 }
