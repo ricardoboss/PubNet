6512b3a2808f5c381e2ed8cfd691b1f2d2d73159
Ricardo Boss <mail@ricardoboss.de>
2025-03-01 01:12:37 +0100

    Added admin service for clients

---
+++ Frontend/PubNet.Client.Abstractions/IAdminService.cs
@@ -0,0 +1,8 @@
+﻿using PubNet.Client.ApiClient.Generated.Models;
+
+namespace PubNet.Client.Abstractions;
+
+public interface IAdminService
+{
+	Task<IdentityDto> CreateAccountAsync(CreateAccountDto data, CancellationToken cancellationToken = default);
+}
