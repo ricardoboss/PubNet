111415a3c4136e7d5920566725e19dadfedce115
Ricardo Boss <mail@ricardoboss.de>
2025-03-19 22:05:56 +0100

    Fixed rebase related build error

---
+++ Backend/PubNet.API/ThisAssembly.cs
@@ -0,0 +1,7 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[SuppressMessage("ReSharper", "CheckNamespace", Justification = "Assembly-level")]
+internal static partial class ThisAssembly
+{
+	public static string MajorMinorPatch { get; } = AssemblyVersion.Split('.').Take(3).Aggregate((a, b) => $"{a}.{b}");
+}
