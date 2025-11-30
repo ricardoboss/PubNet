1b32db495bc0964be131c434cdece004aff3b524
Ricardo Boss <mail@ricardoboss.de>
2025-11-30 10:02:29 +0100

    chore: added migration to make author email non nullable

--- src/PubNet.Database/Migrations/20251130090150_MakeAuthorEmailNonNullable.Designer.cs
+++ src/PubNet.Database/Migrations/20251130090150_MakeAuthorEmailNonNullable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PubNet.Database;
@@ -12,9 +13,11 @@ using PubNet.Database.Models;
 namespace PubNet.Database.Migrations
 {
     [DbContext(typeof(PubNetContext))]
-    partial class PubNetContextModelSnapshot : ModelSnapshot
+    [Migration("20251130090150_MakeAuthorEmailNonNullable")]
+    partial class MakeAuthorEmailNonNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PubNet.Database/Migrations/20251130090150_MakeAuthorEmailNonNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PubNet.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAuthorEmailNonNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Authors",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Authors",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
