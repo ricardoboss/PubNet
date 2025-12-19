ff4f8ddc08c1ac0b2f64217c4790d8c9a20e9ac2
Ricardo Boss <mail@ricardoboss.de>
2025-12-19 01:44:56 +0100

    chore: updated generated files

--- src/PubNet.SDK/Generated/Packages/Item/Versions/Item/Analysis/AnalysisRequestBuilder.cs
+++ src/PubNet.SDK/Generated/Packages/Item/Versions/Item/Analysis/AnalysisRequestBuilder.cs
@@ -37,7 +37,7 @@ namespace PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
         /// <exception cref="global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto401Error">When receiving a 401 status code</exception>
-        /// <exception cref="global::PubNet.SDK.Generated.Models.PackageNotFoundErrorDto">When receiving a 404 status code</exception>
+        /// <exception cref="global::PubNet.SDK.Generated.Models.PackageVersionNotFoundErrorDto">When receiving a 404 status code</exception>
         /// <exception cref="global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto4XXError">When receiving a 4XX status code</exception>
         /// <exception cref="global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto500Error">When receiving a 500 status code</exception>
         /// <exception cref="global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto5XXError">When receiving a 5XX status code</exception>
@@ -54,7 +54,7 @@ namespace PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis
             var errorMapping = new Dictionary<string, ParsableFactory<IParsable>>
             {
                 { "401", global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto401Error.CreateFromDiscriminatorValue },
-                { "404", global::PubNet.SDK.Generated.Models.PackageNotFoundErrorDto.CreateFromDiscriminatorValue },
+                { "404", global::PubNet.SDK.Generated.Models.PackageVersionNotFoundErrorDto.CreateFromDiscriminatorValue },
                 { "4XX", global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto4XXError.CreateFromDiscriminatorValue },
                 { "500", global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto500Error.CreateFromDiscriminatorValue },
                 { "5XX", global::PubNet.SDK.Generated.Packages.Item.Versions.Item.Analysis.PackageVersionAnalysisDto5XXError.CreateFromDiscriminatorValue },
