0ef9901331bdf436b8670d99edc58b488220f971
Ricardo Boss <mail@ricardoboss.de>
2025-12-22 00:16:06 +0100

    feat: added error handling for API errors and documented IAuthenticationService

--- src/PubNet.SDK/Services/ApiAuthenticationService.cs
+++ src/PubNet.SDK/Services/ApiAuthenticationService.cs
@@ -1,6 +1,8 @@
+using Microsoft.Kiota.Abstractions;
 using PubNet.SDK.Abstractions;
 using PubNet.SDK.Exceptions;
 using PubNet.SDK.Generated;
+using PubNet.SDK.Generated.Authentication.Self;
 using PubNet.SDK.Generated.Models;
 
 namespace PubNet.SDK.Services;
@@ -14,55 +16,109 @@ internal sealed class ApiAuthenticationService(
 
 	public async Task<bool> GetRegistrationsEnabledAsync(CancellationToken cancellationToken = default)
 	{
-		return await apiClient.Authentication.RegistrationsEnabled.GetAsync(cancellationToken: cancellationToken) ??
-			false;
+		try
+		{
+			return await apiClient.Authentication.RegistrationsEnabled.GetAsync(cancellationToken: cancellationToken) ??
+				throw new UnexpectedResponseException("Unable to deserialize registrations enabled response");
+		}
+		catch (ApiException e)
+		{
+			throw new UnexpectedResponseException(e);
+		}
 	}
 
-	public async ValueTask<bool> IsAuthenticatedAsync(CancellationToken cancellationToken = default)
+	public async Task<bool> IsAuthenticatedAsync(CancellationToken cancellationToken = default)
 	{
-		return await loginTokenStorage.GetTokenAsync(cancellationToken) is not null;
+		try
+		{
+			return await loginTokenStorage.GetTokenAsync(cancellationToken) is not null;
+		}
+		catch (Exception)
+		{
+			return false;
+		}
 	}
 
-	public async Task<JsonWebTokenResponseDto?> LoginAsync(LoginRequestDto request, CancellationToken cancellationToken = default)
+	public async Task<JsonWebTokenResponseDto> LoginAsync(string email, string password,
+		CancellationToken cancellationToken = default)
 	{
-		return await apiClient.Authentication.Login.PostAsync(request, cancellationToken: cancellationToken);
+		var request = new LoginRequestDto
+		{
+			Email = email,
+			Password = password,
+		};
+
+		try
+		{
+			var response =
+				await apiClient.Authentication.Login.PostAsync(request, cancellationToken: cancellationToken);
+
+			return response ?? throw new UnexpectedResponseException("Unable to deserialize login response");
+		}
+		catch (EmailNotFoundErrorDto e)
+		{
+			throw new InvalidLoginCredentialsException($"E-mail not found: {email}", e);
+		}
+		catch (InvalidPasswordErrorDto e)
+		{
+			throw new InvalidLoginCredentialsException("Wrong password", e);
+		}
+		catch (ApiException e)
+		{
+			throw new UnexpectedResponseException(e);
+		}
 	}
 
 	public async Task LogoutAsync(CancellationToken cancellationToken = default)
 	{
-		await loginTokenStorage.DeleteTokenAsync(cancellationToken);
-
-		_self = null;
+		try
+		{
+			await loginTokenStorage.DeleteTokenAsync(cancellationToken);
+		}
+		finally
+		{
+			_self = null;
+		}
 	}
 
-	public async Task<AuthorDto?> GetSelfAsync(CancellationToken cancellationToken = default)
+	public async Task<AuthorDto> GetSelfAsync(bool forceLoad = false, CancellationToken cancellationToken = default)
 	{
 		if (!await IsAuthenticatedAsync(cancellationToken))
 			throw new AuthenticationRequiredException("Not authenticated");
 
-		if (_self is not null)
+		if (!forceLoad && _self is not null)
 			return _self;
-
-		var maybeAuthor = await apiClient.Authentication.Self.GetAsync(cancellationToken: cancellationToken);
-
-		return _self = maybeAuthor ?? throw new AuthenticationRequiredException("Request failed");
-	}
-
-	public async Task<bool> IsSelfAsync(string? username, CancellationToken cancellationToken = default)
-	{
-		if (username is null) return false;
 
 		try
 		{
-			var self = await GetSelfAsync(cancellationToken);
+			var maybeAuthor = await apiClient.Authentication.Self.GetAsync(cancellationToken: cancellationToken);
 
-			return self?.UserName == username;
+			return _self = maybeAuthor ??
+				throw new UnexpectedResponseException("Failed to deserialize self response");
 		}
-		catch (Exception e)
+		catch (AuthorDto401Error e)
 		{
-			if (e is AuthenticationRequiredException)
-				await LogoutAsync(CancellationToken.None);
+			// token probably expired
+			await LogoutAsync(CancellationToken.None);
 
+			throw new AuthenticationRequiredException("Not authenticated", e);
+		}
+		catch (ApiException e)
+		{
+			throw new UnexpectedResponseException(e);
+		}
+	}
+
+	public async Task<bool> IsSelfAsync(string username, CancellationToken cancellationToken = default)
+	{
+		try
+		{
+			var self = await GetSelfAsync(cancellationToken: cancellationToken);
+
+			return self.UserName == username;
+		}
+		catch (Exception)
+		{
 			return false;
 		}
 	}
